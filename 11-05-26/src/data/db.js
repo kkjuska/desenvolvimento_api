253e87be7a57ebc1ab0e993a5177437b33b0ac44
Lucas Marinho <lucassenatore4@gmail.com>
2026-05-13 15:43:33 -0300

    Update db.js

--- 11-05-26/src/data/db.js
+++ 11-05-26/src/data/db.js
@@ -11,8 +11,7 @@ export const pool = new Pool({
     user: process.env.DB_USER
 })
  
-// the pool will emit an error on behalf of any idle clients
-// it contains if a backend error or network partition happens
+
 pool.on('error', (err, client) => {
   console.error('Unexpected error on idle client', err)
   process.exit(-1)
